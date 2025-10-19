6a8247997c4c9e68b1ec3b1c5cbd43c035247465
RizqyDS <rizqysyahputraa@gmail.com>
2025-10-19 20:50:25 +0700

    update portfolio

--- components/Work/Work.jsx
+++ components/Work/Work.jsx
@@ -55,6 +55,12 @@ const data = [
     title: "Beauty Surgery Website",
   },
   {
+    href: "https://www.triartha.com/",
+    category: "frontend",
+    img: "/assets/work/frontend-5.png",
+    title: "Company Profile Website",
+  },
+  {
     href: "https://storegg-puce.vercel.app",
     category: "frontend",
     img: "/assets/work/frontend-1.png",
